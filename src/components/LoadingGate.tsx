f83b49a2482b8427037691125629cf14bf210ba6
Nathaniel Grossman <nategrossman@mac.com>
2025-01-20 22:18:46 -0800

    add loading gate

---
+++ src/components/LoadingGate.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { useGameStore } from "../store/useGameStore";
+export const LoadingGate: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const loaded = useGameStore.use.loaded();
+  if (!loaded) return <div>Please upload a JSON or DPO file to proceed</div>;
+  return <>{children}</>;
+};
--- src/routes/damage-calculator.tsx
+++ src/routes/damage-calculator.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import DamageCalculator from "../screens/DamageCalculator";
+import { withLoadingGate } from "../utils/withLoadingGate";
 
 export const Route = createFileRoute("/damage-calculator")({
-  component: DamageCalculator,
+  component: withLoadingGate(DamageCalculator),
 });
